3d472c63006e158fc53231a72584404f0c78f194
Josh King <webposer@gmail.com>
2016-10-28 23:42:06 -0600

    gulp working now. Base bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
-var gulp          = require('gulp');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('styles', function() {
-    gulp.src('src/app/styles/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('src/'))
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('src/app/styles/**/*.scss',['style']);
+    gulp.watch('scss/**/*.scss',['sass']);
 });
